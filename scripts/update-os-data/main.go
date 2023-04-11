d28dd7524a11ee2fb8399997c38c03061412d6a1
whoisnian <zhuchangbao1998@gmail.com>
2023-04-12 00:10:30 +0800

    ADD: scripts/update-os-data

---
+++ scripts/update-os-data/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
+	"encoding/json"
+	"hash"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/whoisnian/glb/logger"
+	"github.com/whoisnian/virt-launcher/data"
+	"github.com/whoisnian/virt-launcher/image"
+)
+
+var regMap = map[string]*regexp.Regexp{
+	"archlinux":      regexp.MustCompile(`href="v(\d+.\d+)/"`),
+	"centos7.0":      regexp.MustCompile(`href="CentOS-7-x86_64-GenericCloud-(\d+).qcow2"`),
+	"centos-stream8": regexp.MustCompile(`href="CentOS-Stream-GenericCloud-8-(\d+.\d+).x86_64.qcow2"`),
+	"centos-stream9": regexp.MustCompile(`href="CentOS-Stream-GenericCloud-9-(\d+.\d+).x86_64.qcow2"`),
+	"debian10":       regexp.MustCompile(`href="(\d+-\d+)/"`),
+	"debian11":       regexp.MustCompile(`href="(\d+-\d+)/"`),
+	"debian12":       regexp.MustCompile(`href="(\d+-\d+)/"`),
+	"ubuntu18.04":    regexp.MustCompile(`href="(\d+)/"`),
+	"ubuntu20.04":    regexp.MustCompile(`href="(\d+)/"`),
+	"ubuntu22.04":    regexp.MustCompile(`href="(\d+)/"`),
+}
+
+func main() {
+	files, err := data.FS.ReadDir(data.OsDir)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	logger.Info("Found ", len(files), " os files")
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		logger.Info("Check for updates '", file.Name(), "'...")
+		content, err := data.FS.ReadFile(filepath.Join(data.OsDir, file.Name()))
+		if err != nil {
+			logger.Fatal(err)
+		}
+
+		o := &image.Os{}
+		err = json.Unmarshal(content, o)
+		if err != nil {
+			logger.Fatal(err)
+		}
+
+		if updateOsData(o) {
+			logger.Info("Update os file: ", file.Name())
+			fi, err := os.Create(filepath.Join("data", data.OsDir, file.Name()))
+			if err != nil {
+				logger.Fatal(err)
+			}
+			defer fi.Close()
+
+			enc := json.NewEncoder(fi)
+			enc.SetIndent("", "  ")
+			if err = enc.Encode(o); err != nil {
+				logger.Fatal(err)
+			}
+		}
+	}
+}
+
+func updateOsData(o *image.Os) bool {
+	reg, ok := regMap[o.Name]
+	if !ok {
+		logger.Fatal("Unknown os name")
+	}
+
+	latestV := fetchLatestVersion(o.Upstream, reg)
+	if latestV <= o.Version {
+		return false
+	}
+	logger.Info("Found newer version: ", o.Version, " => ", latestV)
+
+	for i := range o.Images {
+		o.Images[i].Url = strings.ReplaceAll(o.Images[i].Url, o.Version, latestV)
+		if strings.HasPrefix(o.Images[i].Hash, "https://") || strings.HasPrefix(o.Images[i].Hash, "http://") {
+			o.Images[i].Hash = strings.ReplaceAll(o.Images[i].Hash, o.Version, latestV)
+		} else if strings.HasPrefix(o.Images[i].Hash, "sha256sum:") {
+			o.Images[i].Hash = "sha256sum:" + remoteHashFrom(o.Images[i].Url, sha256.New())
+		} else if strings.HasPrefix(o.Images[i].Hash, "sha512sum:") {
+			o.Images[i].Hash = "sha512sum:" + remoteHashFrom(o.Images[i].Url, sha512.New())
+		}
+	}
+	o.Version = latestV
+	return true
+}
+
+func remoteHashFrom(url string, hasher hash.Hash) string {
+	resp, err := http.Get(url)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	_, err = io.CopyBuffer(hasher, resp.Body, make([]byte, 4*1024*1024))
+	if err != nil {
+		logger.Fatal(err)
+	}
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func fetchLatestVersion(upstream string, reg *regexp.Regexp) (version string) {
+	resp, err := http.Get(upstream)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	scanner := bufio.NewScanner(resp.Body)
+	for scanner.Scan() {
+		if matches := reg.FindAllSubmatch(scanner.Bytes(), -1); len(matches) > 0 {
+			for _, match := range matches {
+				if len(match) > 1 && string(match[1]) > version {
+					version = string(match[1])
+				}
+			}
+		}
+	}
+	return
+}
